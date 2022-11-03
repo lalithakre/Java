fa82e3f0aebd058330c793fc6c93d448309a60aa
Lalit Thakre <lalit03thakre@gmail.com>
2022-11-03 22:21:01 +0530

    JAVA

---
+++ StringMethods.java
@@ -0,0 +1,17 @@
+public class StringMethods {
+    public static void main(String args[]) {
+        int n;
+        String s = "Java programming", t = "", u = "";
+        System.out.println(s);
+        n = s.length();
+        System.out.println("Number of characters = " + n);
+        // Replace characters in string
+        t = s.replace("Java", "C++");
+        System.out.println(s);
+        System.out.println(t);
+        // Concatenating string with another string
+        u = s.concat(" is fun");
+        System.out.println(s);
+        System.out.println(u);
+    }
+}
